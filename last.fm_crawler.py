29a9b5b00030e87120e2f0b273b02fdae6e4022e
Bartosz Nowakowski <bartosz.alfred.nowakowski@gmail.com>
2019-10-23 16:34:53 +0200

    First page loaded into a list

--- last.fm_crawler.py
+++ last.fm_crawler.py
@@ -12,7 +12,45 @@ uClient.close()
 library_soup = soup(library_html, 'html.parser')
 
 # bs4.element.ResultSet /with all containers
-library_page = library_soup.findAll("tr", {'class': 'chartlist-row'})
+library_page = library_soup.findAll('tr', {'class': 'chartlist-row'})
 
+dict_months = {
+    'Jan':1,
+    'Feb':2,
+    'Mar':3,
+    'Apr':4,
+    'May':5,
+    'Jun':6,
+    'Jul':7,
+    'Aug':8,
+    'Sep':9,
+    'Oct':10,
+    'Nov':11,
+    'Dec':12}
 
-library_page.findAll("td", {'class': 'chartlist-artist'})
+def DatestampParser(datestamp):
+    
+    datestamp_list = datestamp.split(' ')
+    
+    day = int(datestamp_list[1])
+    month = dict_months[str(datestamp_list[2])]
+    year = int(datestamp_list[3][:4])  
+    #hour
+    #minute
+
+data = []
+
+for container in library_page:
+    Artist_full = str(container.findAll('td', {'class': 'chartlist-name'}))
+    Artist_short = Artist_full[(Artist_full.find('title=') + 6):(len(Artist_full)-11)]
+    Artist = Artist_short[Artist_short.find('>')+1:]
+    
+    Track_full = str(container.findAll('td', {'class': 'chartlist-artist'}))
+    Track_short = Track_full[(Track_full.find('title=')+6):(len(Track_full)-11)]
+    Track = Track_short[Track_short.find('>')+1:]
+    
+    Timestamp_full= str(container)[str(container).find('timestamp')+54:len(str(container))-13]
+    Timestamp_short = Timestamp_full[:Timestamp_full.find('>')-1]
+    Timestamp = DatestampParser(Timestamp_short)
+
+    data.append([Artist, Track, Timestamp])
