7b77d23cfec9ffb67158e0147e12cadf2ade7210
Bartosz Nowakowski <bartosz.alfred.nowakowski@gmail.com>
2019-10-25 10:56:20 +0200

    Code cleaning

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # last.fm crawler
 
-Intention of thic project is to create Python crawler which will scrape all historic records into CSV file:
+Intention of thic project is to create Python crawler which will scrape all historic records of my last.fm library into CSV file:
 - Artist
 - Title
 - Timestamp
 
-In the next step once crawler is working, I'd like to add next level crawler to enter each song's website to scrape:
+In the next step once crawler is working, I'd like to add next level crawler to enter each song's website to scrape its:
 - Album
 - Length
 - Tags
--- last.fm_crawler.py
+++ last.fm_crawler.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup as soup
 from urllib.request import urlopen as uReq
 import pandas as pd
+from StringParsers import DateParser, TimeParser
 
 user = 'takieo'
 lastfm = f'https://www.last.fm/user/{user}/library'
@@ -13,44 +14,11 @@ uClient.close()
 library_soup = soup(library_html, 'html.parser')
 
 # number of pages in library
-library_pages = library_soup.findAll('li', {'class':'pagination-page'})
-all_pages_long = str(library_pages[::-1][0])
-all_pages_short = all_pages_long[all_pages_long.find('page=')+5:]
-all_pages = all_pages_short[:all_pages_short.find('"')]
-
-dict_months = {'Jan':'01', 'Feb':'02', 'Mar':'03',
-               'Apr':'04', 'May':'05', 'Jun':'06',
-               'Jul':'07', 'Aug':'08', 'Sep':'09',
-               'Oct':'10', 'Nov':'11', 'Dec':'12'}
-
-def DateParser(datestamp):
-    
-    datestamp_list = datestamp.split(' ')
-    
-    day = int(datestamp_list[1])
-    month = dict_months[str(datestamp_list[2])]
-    year = int(datestamp_list[3][:4])  
-    
-    day = '0' + str(day) if day<10 else str(day)
-
-    return f'{year}-{month}-{day}'
-
-def TimeParser(datestamp):
-
-    datestamp_list = datestamp.split(' ')
-    time_long = str(datestamp_list[4])[:-2].split(":")
-    
-    hour = int(time_long[0])
-    minute = str(time_long[1])
-    
-    if str(datestamp_list[4])[-2:] == 'pm': hour += 12
-    hour = '0' + str(hour) if hour<10 else str(hour)
-
-    return f'{hour}:{minute}'
+all_pages = library_soup.findAll('li', {'class':'pagination-page'})[::-1][0].get_text().strip('\n')
 
 data = []
 
-for page in range(1, int(all_pages)+1):
+for page in range(1, 11): #int(all_pages)+1
 
     lastfm = f'https://www.last.fm/user/{user}/library?page={page}'
 
@@ -58,26 +26,18 @@ for page in range(1, int(all_pages)+1):
     library_html = uClient.read()
     uClient.close()
 
-    # bs4.BeautifulSoup /with whole website
     library_soup = soup(library_html, 'html.parser')
-
-    # bs4.element.ResultSet /with all containers
     library_page = library_soup.findAll('tr', {'class': 'chartlist-row'})
 
     for container in library_page:
 
-        Track_full = str(container.findAll('td', {'class': 'chartlist-name'}))
-        Track_short = Track_full[(Track_full.find('title=') + 6):(len(Track_full)-11)]
-        Track = Track_short[Track_short.find('>')+1:].replace('&amp;', '&')
-    
-        Artist_full = str(container.findAll('td', {'class': 'chartlist-artist'}))
-        Artist_short = Artist_full[(Artist_full.find('title=')+6):(len(Artist_full)-11)]
-        Artist = Artist_short[Artist_short.find('>')+1:].replace('&amp;', '&')
-
-        Timestamp_full= str(container)[str(container).find('timestamp')+54:len(str(container))-13]
-        Timestamp_short = Timestamp_full[:Timestamp_full.find('>')-1]
-        Date = DateParser(Timestamp_short)
-        Time = TimeParser(Timestamp_short)
+        Track = container.findAll('td', {'class': 'chartlist-name'})[0].get_text().strip('\n')
+        Artist = container.findAll('td', {'class': 'chartlist-artist'})[0].get_text().strip('\n')
+        
+        Timestamp = container.findAll('td', {'class': 'chartlist-timestamp chartlist-timestamp--lang-en'})[0].find('span')['title']
+        
+        Date = DateParser(Timestamp)
+        Time = TimeParser(Timestamp)
 
         data.append([Artist, Track, Date, Time])
 
